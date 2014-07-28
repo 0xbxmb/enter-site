8468dab8422e66374e3cfd5ebaebcad5fb346a4c
bombinmybag <bombinmybag@gmail.com>
2014-07-28 18:10:45 +0600

    day 13

--- js/main.js
+++ js/main.js
@@ -184,4 +184,106 @@ $(function () {
     });
 
 
+    var width = 960,
+        height = 500;
+
+    // Setting color domains(intervals of values) for our map
+
+    var color_domain = [0, 700]
+    var color = d3.scale.threshold()
+        .domain(color_domain)
+        .range(["#4092ff", "#ff4e40"]);
+
+    var div = d3.select("#map-wrapper").append("div")
+        .attr("class", "tooltip")
+        .style("opacity", 0);
+
+    var svg = d3.select("#map-wrapper").append("svg")
+        .attr("width", width)
+        .attr("height", height)
+        .style("margin", "10px auto");
+
+    var projection = d3.geo.albers()
+        .rotate([-105, 0])
+        .center([-10, 65])
+        .parallels([52, 64])
+        .scale(700)
+        .translate([width / 2, height / 2]);
+
+    var path = d3.geo.path().projection(projection);
+
+    //Reading map file and data
+
+    queue()
+        .defer(d3.json, "russia_1e-7sr.json")
+        .await(ready);
+
+    //Start of Choropleth drawing
+
+    function ready(error, map, data) {
+
+        debugger;
+
+        var rateById = {};
+        var nameById = {};
+
+
+        //Drawing Choropleth
+
+        svg.append("g")
+            .attr("class", "region")
+            .selectAll("path")
+            .data(topojson.object(map, map.objects.russia).geometries)
+            //.data(topojson.feature(map, map.objects.russia).features) <-- in case topojson.v1.js
+            .enter().append("path")
+            .attr("d", path)
+            .style("fill", "#FFF")
+            .style("opacity", 0.8)
+
+/*
+            //Adding mouseevents
+            .on("mouseover", function(d) {
+                d3.select(this).transition().duration(300).style("opacity", 1);
+                div.transition().duration(300)
+                    .style("opacity", 1)
+                div.text(nameById[d.properties.region] + " : " + rateById[d.properties.region])
+                    .style("left", (d3.event.pageX) + "px")
+                    .style("top", (d3.event.pageY -30) + "px");
+            })
+            .on("mouseout", function() {
+                d3.select(this)
+                    .transition().duration(300)
+                    .style("opacity", 0.8);
+                div.transition().duration(300)
+                    .style("opacity", 0);
+            })
+*/
+
+
+
+        // Adding cities on the map
+
+        d3.tsv("cities.tsv", function(error, data) {
+            var city = svg.selectAll("g.city")
+                .data(data)
+                .enter()
+                .append("g")
+                .attr("class", "city")
+                .attr("transform", function(d) { return "translate(" + projection([d.lon, d.lat]) + ")"; })
+                .on("click", function(d){
+                    alert(d);
+                });
+
+            city.append("circle")
+                .attr("r", 3)
+                .style("fill", "#F60")
+                .style("opacity", 0.75);
+
+            city.append("text")
+                .attr("x", 5)
+                .text(function(d) { return d.City; });
+        });
+
+    }; // <-- End of Choropleth drawing
+
 });
